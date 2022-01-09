119aeddab63c74d1aad9bd21d9fc8f9ad8861097
Salah Abdinoor <Salah.Abdinoor@theservicecorporation.com>
2022-01-09 12:01:27 +0100

    finished task-tracker - Moving to github

--- README.md
+++ README.md
@@ -68,3 +68,7 @@ This section has moved here: [https://facebook.github.io/create-react-app/docs/d
 ### `npm run build` fails to minify
 
 This section has moved here: [https://facebook.github.io/create-react-app/docs/troubleshooting#npm-run-build-fails-to-minify](https://facebook.github.io/create-react-app/docs/troubleshooting#npm-run-build-fails-to-minify)
+
+### `json-server -p 5000 --watch db.json` Starts json server from db.json
+
+### `serve json` Starts Deploys on the network
--- src/App.js
+++ src/App.js
@@ -1,25 +1,127 @@
-import logo from './logo.svg';
-import './App.css';
+/* eslint-disable no-unused-vars */
+/* eslint-disable prettier/prettier */
+import { useState, useEffect } from "react"
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom"
+import Header from "./components/Header"
+import AddTask from "./components/AddTask"
+import Tasks from "./components/Tasks"
+import About from "./components/About"
+import Footer from "./components/Footer"
 
 function App() {
+  const [showAddTask, setShowAddTask] = useState(false)
+
+  // Default Set Task
+  const [tasks, setTasks] = useState([])
+
+  // get Data from server
+  useEffect(() => {
+    const getTasks = async () => {
+      const tasksFromServer = await fetchTasks()
+      setTasks(tasksFromServer)
+    }
+
+    getTasks()
+  }, [])
+
+  // fetch all tasks
+  const fetchTasks = async () => {
+    const respone = await fetch("http://localhost:5000/tasks")
+    const data = await respone.json()
+
+    return data
+  }
+
+  // fetch single task
+  const fetchTask = async (id) => {
+    const respone = await fetch(`http://localhost:5000/tasks/${id}`)
+    const data = await respone.json()
+
+    return data
+  }
+
+  // Add Task
+  const addTask = async (task) => {
+    const response = await fetch("http://localhost:5000/tasks", {
+      method: "POST",
+      headers: {
+        "Content-type": "application/json",
+      },
+      body: JSON.stringify(task),
+    })
+
+    const data = await response.json()
+
+    setTasks([...tasks, data])
+  }
+
+  // Delete Task
+  const deleteTask = async (id) => {
+    await fetch(`http://localhost:5000/tasks/${id}`, {
+      method: "DELETE",
+    })
+
+    setTasks(tasks.filter((task) => task.id !== id))
+  }
+
+  // Toggle Reminder
+  const toggleReminder = async (id) => {
+    const taskToToggle = await fetchTask(id)
+    const updTask = { ...taskToToggle, reminder: !taskToToggle.reminder }
+
+    const response = await fetch(`http://localhost:5000/tasks/${id}`, {
+      method: "PUT",
+      headers: {
+        "Content-type": "application/json",
+      },
+      body: JSON.stringify(updTask),
+    })
+
+    const data = await response.json()
+
+    setTasks(
+      tasks.map((task) =>
+        task.id === id ? { ...task, reminder: data.reminder } : task
+      )
+    )
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <Router>
+      <div className="container">
+        <Header
+          title="Task Tracker"
+          showAdd={showAddTask}
+          onAdd={() => setShowAddTask(!showAddTask)}
+        />
+        {showAddTask && <AddTask onAdd={addTask} />}
+
+        <Routes>
+          <Route
+            path="/"
+            element={
+              tasks.length > 0 ? (
+                <Tasks
+                  tasks={tasks}
+                  onToggle={toggleReminder}
+                  onDelete={deleteTask}
+                />
+              ) : (
+                <h4>No Tasks To Show</h4>
+              )
+            }
+          />
+        </Routes>
+
+        <Routes>
+          <Route path="/about" element={<About />} />
+        </Routes>
+        <Routes>
+          <Route path="/" element={<Footer />} />
+        </Routes>
+      </div>
+    </Router>
+  )
 }
 
-export default App;
+export default App
